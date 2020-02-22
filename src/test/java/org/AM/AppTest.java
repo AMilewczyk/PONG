368656962683ed24a35c70f59b157a3ef135973b
Andrzej <andrzej.milewczyk1@gmail.com>
2020-02-22 12:32:19 +0100

    add test catalog

---
+++ src/test/java/org/AM/AppTest.java
@@ -0,0 +1,52 @@
+package org.AM;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+    @org.junit.jupiter.api.Test
+    void launch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testLaunch() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void init() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void stop() {
+    }
+
+    @Test
+    void getHostServices() {
+    }
+
+    @Test
+    void getParameters() {
+    }
+
+    @Test
+    void notifyPreloader() {
+    }
+
+    @Test
+    void getUserAgentStylesheet() {
+    }
+
+    @Test
+    void setUserAgentStylesheet() {
+    }
+
+    @Test
+    void start() {
+    }
+}
